57fd58b353f610379f27f7693dd8ce048e94889c
rajusidda <rajusidda82@gmail.com>
2019-07-03 16:14:57 +0530

    demo server for micro services test

---
+++ src/main/java/com/server/SpringBootDemoServer/SpringBootDemoServerApplication.java
@@ -0,0 +1,15 @@
+package com.server.SpringBootDemoServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringBootDemoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoServerApplication.class, args);
+	}
+
+}
